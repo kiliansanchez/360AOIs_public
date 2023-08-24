03a9da991fbbdd2eace902e8e8b5501f22c6d620
DESKTOP-LQJHLP7\Alex <kilian.sanchezholguin@gmail.com>
2023-08-24 18:12:04 +0200

    some more xml comments

--- Assets/Scripts/AOI Management/InputfieldLayoutHelper.cs
+++ Assets/Scripts/AOI Management/InputfieldLayoutHelper.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Attached to the textmeshpro object of the list entries in the AOI list. Helps with fixing the random jumps the text fields make when editing.
+/// </summary>
+
 public class InputfieldLayoutHelper : MonoBehaviour
 {
     public TMPro.TMP_InputField InputField;
@@ -24,7 +28,7 @@ public class InputfieldLayoutHelper : MonoBehaviour
 
     }
 
-    //called from ui
+    //Callback for TextField (TMP) OnDeselect and OnEndEdit Callbacks. Resets position of label (TMP) and Caret after editing is complete.
     public void Reset()
     {
         // reset text rect
@@ -36,21 +40,41 @@ public class InputfieldLayoutHelper : MonoBehaviour
         SetRight(_caretRect, RightMargin);
     }
 
+    /// <summary>
+    /// Helper to set "left" property off RectTransform similar to how you would do it in the UnityEditor.
+    /// </summary>
+    /// <param name="rt"></param>
+    /// <param name="left"></param>
     public void SetLeft(RectTransform rt, float left)
     {
         rt.offsetMin = new Vector2(left, rt.offsetMin.y);
     }
 
+    /// <summary>
+    /// Helper to set "right" property off RectTransform similar to how you would do it in the UnityEditor.
+    /// </summary>
+    /// <param name="rt"></param>
+    /// <param name="right"></param>
     public void SetRight(RectTransform rt, float right)
     {
         rt.offsetMax = new Vector2(-right, rt.offsetMax.y);
     }
 
+    /// <summary>
+    /// Helper to set "top" property off RectTransform similar to how you would do it in the UnityEditor.
+    /// </summary>
+    /// <param name="rt"></param>
+    /// <param name="top"></param>
     public void SetTop(RectTransform rt, float top)
     {
         rt.offsetMax = new Vector2(rt.offsetMax.x, -top);
     }
 
+    /// <summary>
+    /// Helper to set "bottom" property off RectTransform similar to how you would do it in the UnityEditor.
+    /// </summary>
+    /// <param name="rt"></param>
+    /// <param name="bottom"></param>
     public void SetBottom(RectTransform rt, float bottom)
     {
         rt.offsetMin = new Vector2(rt.offsetMin.x, bottom);
